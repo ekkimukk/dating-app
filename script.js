4f3bcebe45f20701a90a0f8820b5eea707c72d98
ekki <songbirdwhosing@gmail.com>
2026-05-14 12:52:52 +0300

    для молевой

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ document.getElementById('no-button').addEventListener('mouseenter', () => {
   document.getElementById('no-button').style.left = `${x}px`;
   document.getElementById('no-button').style.top = `${y}px`;
 });
-
-
-document.getElementById('yes-button1')
-  .addEventListener('click', () => location = 'you-pay.html');
-
-
-document.getElementById('yes-button2')
-  .addEventListener('click', () => location = 'happy-page.html');
